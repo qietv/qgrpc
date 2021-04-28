f40c2654b093bbc7fa1f069e2ae7d72a41af2f78
guohao <i@icool.io>
2021-04-28 20:59:48 +0800

    improve trace

--- interceptor.go
+++ interceptor.go
@@ -2,115 +2,191 @@ package qgrpc
 
 import (
 	"context"
-	"fmt"
+	"encoding/base64"
 	"github.com/hanskorg/logkit"
+	"github.com/opentracing/opentracing-go"
+	"github.com/opentracing/opentracing-go/ext"
+	"github.com/opentracing/opentracing-go/log"
+	"github.com/uber/jaeger-client-go"
+	"github.com/uber/jaeger-client-go/config"
+	"github.com/uber/jaeger-client-go/zipkin"
+	"github.com/uber/jaeger-lib/metrics"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/grpclog"
 	"google.golang.org/grpc/metadata"
-	"google.golang.org/grpc/peer"
-	"google.golang.org/grpc/status"
 	"io"
-	"net"
-	"os"
-	"runtime/debug"
+	"strings"
 	"time"
 )
 
-// NewLoggingInterceptor create log interceptor
-// format {time}\t{origin}\t{method}\t{http code}\t{status.code}\t{status.message}
-func NewLoggingInterceptor(logfile string) grpc.UnaryServerInterceptor {
-	accessLogger, err := logkit.NewFileLogger(logfile, "", time.Second, uint64(1204*1024*1800), 0)
-	if err != nil {
-		println("logger conf fail, %s", err.Error())
-	}
-	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
-		var (
-			startTime time.Time
-			code      int32
-			message   string
-			remoteIp  string
-			ret       *status.Status
-			durTime   float32
-		)
-		if accessLogger == nil {
-			return handler(ctx, req)
-		}
-		startTime = time.Now()
-		resp, err = handler(ctx, req)
-		if err != nil {
-			ret = status.Convert(err)
-			code = int32(ret.Code())
-			message = ret.Message()
-		}
-
-		if pr, ok := peer.FromContext(ctx); ok {
-			if tcpAddr, ok := pr.Addr.(*net.TCPAddr); ok {
-				remoteIp = tcpAddr.IP.String()
-			} else {
-				remoteIp = pr.Addr.String()
-			}
-		}
-
-		if md, ok := metadata.FromIncomingContext(ctx); !ok {
-			rips := md.Get("x-forward-for")
-			if len(rips) != 0 {
-				remoteIp = rips[0]
-			}
-		}
-		if remoteIp == "" {
-			remoteIp = "-"
-		}
-		if message == "" {
-			message = "-"
-		}
-		durTime = float32(time.Since(startTime).Microseconds()) / 1000.0
-		if _, err := accessLogger.Write([]byte(fmt.Sprintf("%s\t%s\t%s\t%s\t%-.3f\t%d\t%s\n", startTime.Format(time.RFC3339), remoteIp, info.FullMethod, "-", durTime, code, message))); err != nil {
-			println("write access log fail, %s", err.Error())
-		}
-		return resp, err
-	}
+//MDReaderWriter metadata Reader and Writer
+type MDReaderWriter struct {
+	metadata.MD
 }
 
-// NewRecoveryInterceptor grpc server ServerInterceptor
-// create union revovery handler for qietv gRPC sevice
-func NewRecoveryInterceptor(logfile string) grpc.UnaryServerInterceptor {
-	var (
-		errorLogger io.Writer
-		err         error
-		startTime   time.Time
-		remoteIp    string
+// Set implements Set() of opentracing.TextMapWriter
+func (c MDReaderWriter) Set(key, val string) {
+	key = strings.ToLower(key)
+	if strings.HasSuffix(key, "-bin") {
+		val := base64.StdEncoding.EncodeToString([]byte(val))
+		val = string(val)
+	}
+	c.MD[key] = append(c.MD[key], val)
+}
+
+func (c MDReaderWriter) ForeachKey(handler func(key, val string) error) error {
+	for k, vs := range c.MD {
+		for _, v := range vs {
+			if err := handler(k, v); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+// DialOption grpc client option
+func DialOption(tracer opentracing.Tracer) grpc.DialOption {
+	return grpc.WithUnaryInterceptor(clientInterceptor(tracer))
+}
+
+// NewTracer NewJaegerTracer for current service
+func NewTracer(serviceName string) (tracer opentracing.Tracer, closer io.Closer, err error) {
+	tracer = opentracing.GlobalTracer()
+	return
+}
+
+func NewJaegerTracer(serviceName string, jagentHost string) (tracer opentracing.Tracer, closer io.Closer, err error) {
+	cfg := config.Configuration{
+		Headers: &jaeger.HeadersConfig{
+			TraceContextHeaderName:   "x-trace-id",
+			TraceBaggageHeaderPrefix: "x-qt-",
+		},
+		ServiceName: serviceName,
+		Sampler: &config.SamplerConfig{
+			Type:  jaeger.SamplerTypeConst,
+			Param: 1,
+		},
+	}
+	if jagentHost != "" {
+		cfg.Reporter = &config.ReporterConfig{
+			LogSpans:            true,
+			BufferFlushInterval: 1 * time.Second,
+			LocalAgentHostPort:  jagentHost,
+		}
+	}
+	zkPropagator := zipkin.NewZipkinB3HTTPHeaderPropagator()
+	tracer, closer, err = cfg.NewTracer(
+		config.Logger(jaeger.NullLogger),
+		config.Metrics(metrics.NullFactory),
+		config.Extractor(opentracing.HTTPHeaders, zkPropagator),
+		config.Injector(opentracing.HTTPHeaders, zkPropagator),
+		config.ZipkinSharedRPCSpan(true),
 	)
-
-	if logfile != "" {
-		errorLogger, err = logkit.NewFileLogger(logfile, "", time.Second, uint64(1204*1024*1800), 0)
-	} else {
-		errorLogger, err = os.Open("/dev/stderr")
-	}
 	if err != nil {
-		println("logger conf fail, %s", err.Error())
+		grpclog.Errorf("Could not initialize jaeger tracer: %s", err.Error())
+		return
 	}
-	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+	return
+}
 
-		defer func() {
-			if r := recover(); r != nil {
-				if _, err := errorLogger.Write([]byte(fmt.Sprintf("%s\t%s\t%s\n fatal ===> %s %s \n===============\n", startTime.Format(time.RFC3339), remoteIp, info.FullMethod, r, debug.Stack()))); err != nil {
-					println("write access log fail, %s", err.Error())
-				}
-				err = status.Errorf(codes.Internal, "fatal err: %+v", r)
+type clientSpanTagKey struct{}
+
+//clientInterceptor grpc client wrapper
+func clientInterceptor(tracer opentracing.Tracer) grpc.UnaryClientInterceptor {
+	if tracer == nil {
+		zkPropagator := zipkin.NewZipkinB3HTTPHeaderPropagator()
+		tracer, _, _ = config.Configuration{Headers: &jaeger.HeadersConfig{
+			TraceContextHeaderName:   "x-trace-id",
+			TraceBaggageHeaderPrefix: "x-qt-",
+		}}.NewTracer(
+			config.Logger(jaeger.NullLogger),
+			config.Metrics(metrics.NullFactory),
+			config.Extractor(opentracing.HTTPHeaders, zkPropagator),
+			config.Injector(opentracing.HTTPHeaders, zkPropagator),
+		)
+	}
+
+	return func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+		var (
+			parentCtx opentracing.SpanContext
+			//uid       int64
+			//gateway   net.IP
+			err    error
+			tracer opentracing.Tracer
+		)
+		if QTracer != nil {
+			tracer = QTracer
+		} else {
+			tracer = opentracing.GlobalTracer()
+		}
+		parentSpan := opentracing.SpanFromContext(ctx)
+		if parentSpan != nil {
+			parentCtx = parentSpan.Context()
+		}
+		traceOpts := []opentracing.StartSpanOption{
+			opentracing.ChildOf(parentCtx),
+			ext.SpanKindRPCClient,
+		}
+		if tagx := ctx.Value(clientSpanTagKey{}); tagx != nil {
+			if opt, ok := tagx.(opentracing.StartSpanOption); ok {
+				traceOpts = append(traceOpts, opt)
 			}
-		}()
-		return handler(ctx, req)
+		}
+		span := tracer.StartSpan(method, traceOpts...)
+		defer span.Finish()
+
+		md, ok := metadata.FromOutgoingContext(ctx)
+		if !ok {
+			md = metadata.New(nil)
+		} else {
+			md = md.Copy()
+		}
+		mdWriter := MDReaderWriter{md}
+		if err = tracer.Inject(span.Context(), opentracing.HTTPHeaders, mdWriter); err != nil {
+			span.LogFields(log.String("inject-error", err.Error()))
+		}
+		newCtx := opentracing.ContextWithSpan(metadata.NewOutgoingContext(ctx, md), span)
+		err = invoker(newCtx, method, req, reply, cc, opts...)
+		if err != nil {
+			span.LogFields(log.String("call-error", err.Error()))
+		}
+		return err
 	}
 }
 
-// NewTracerInterceptor grpc server ServerInterceptor
-func NewTracerInterceptor(serviceName string, agentHost string) grpc.UnaryServerInterceptor {
-	tracer, _, err := NewTracer(serviceName, agentHost)
-	if err != nil {
-		return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
-			println("tracer setup fail, %s", err.Error())
-			return handler(ctx, req)
+//serverInterceptor grpc server wrapper
+func serverInterceptor(tracer opentracing.Tracer, withError bool) grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo,
+		handler grpc.UnaryHandler) (resp interface{}, err error) {
+		var (
+			md   metadata.MD
+			ok   bool
+			span opentracing.Span
+		)
+
+		md, ok = metadata.FromIncomingContext(ctx)
+		if !ok {
+			md = metadata.New(nil)
 		}
+		parentSpanContext, err := tracer.Extract(opentracing.HTTPHeaders, MDReaderWriter{md})
+		if err != nil && err != opentracing.ErrSpanContextNotFound {
+			opentracing.SpanFromContext(ctx)
+			logkit.Errorf("extract from metadata err: %v", err)
+		}
+		span = tracer.StartSpan(
+			info.FullMethod,
+			ext.RPCServerOption(parentSpanContext),
+			opentracing.Tag{Key: string(ext.Component), Value: "gRPC"},
+			ext.SpanKindRPCServer,
+		)
+		defer span.Finish()
+		ctx = opentracing.ContextWithSpan(ctx, span)
+
+		resp, err = handler(ctx, req)
+		if err != nil && withError {
+			span.LogFields(log.Object("error", err))
+		}
+		return
 	}
-	return serverInterceptor(tracer)
 }
--- tracer.go
+++ tracer.go
@@ -2,145 +2,127 @@ package qgrpc
 
 import (
 	"context"
+	"fmt"
 	"github.com/hanskorg/logkit"
 	"github.com/opentracing/opentracing-go"
-	"github.com/opentracing/opentracing-go/ext"
-	"github.com/opentracing/opentracing-go/log"
-	"github.com/uber/jaeger-client-go"
-	jaegercfg "github.com/uber/jaeger-client-go/config"
-	"github.com/uber/jaeger-lib/metrics/prometheus"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/peer"
+	"google.golang.org/grpc/status"
 	"io"
-	"strings"
+	"net"
+	"os"
+	"runtime/debug"
 	"time"
 )
 
-//MDReaderWriter metadata Reader and Writer
-type MDReaderWriter struct {
-	metadata.MD
-}
+var (
+	QTracer opentracing.Tracer
+)
 
-// Set implements Set() of opentracing.TextMapWriter
-func (c MDReaderWriter) Set(key, val string) {
-	key = strings.ToLower(key)
-	c.MD[key] = append(c.MD[key], val)
-}
+// NewLoggingInterceptor create log interceptor
+// format {time}\t{origin}\t{method}\t{http code}\t{status.code}\t{status.message}
+func NewLoggingInterceptor(logfile string) grpc.UnaryServerInterceptor {
+	accessLogger, err := logkit.NewFileLogger(logfile, "", time.Second, uint64(1204*1024*1800), 0)
+	if err != nil {
+		println("logger conf fail, %s", err.Error())
+	}
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+		var (
+			startTime time.Time
+			code      int32
+			message   string
+			remoteIp  string
+			ret       *status.Status
+			durTime   float32
+		)
+		if accessLogger == nil {
+			return handler(ctx, req)
+		}
+		startTime = time.Now()
+		resp, err = handler(ctx, req)
+		if err != nil {
+			ret = status.Convert(err)
+			code = int32(ret.Code())
+			message = ret.Message()
+		}
 
-func (c MDReaderWriter) ForeachKey(handler func(key, val string) error) error {
-	for k, vs := range c.MD {
-		for _, v := range vs {
-			if err := handler(k, v); err != nil {
-				return err
+		if pr, ok := peer.FromContext(ctx); ok {
+			if tcpAddr, ok := pr.Addr.(*net.TCPAddr); ok {
+				remoteIp = tcpAddr.IP.String()
+			} else {
+				remoteIp = pr.Addr.String()
 			}
 		}
+
+		if md, ok := metadata.FromIncomingContext(ctx); !ok {
+			rips := md.Get("x-forward-for")
+			if len(rips) != 0 {
+				remoteIp = rips[0]
+			}
+		}
+		if remoteIp == "" {
+			remoteIp = "-"
+		}
+		if message == "" {
+			message = "-"
+		}
+		durTime = float32(time.Since(startTime).Microseconds()) / 1000.0
+		if _, err := accessLogger.Write([]byte(fmt.Sprintf("%s\t%s\t%s\t%s\t%-.3f\t%d\t%s\n", startTime.Format(time.RFC3339), remoteIp, info.FullMethod, "-", durTime, code, message))); err != nil {
+			println("write access log fail, %s", err.Error())
+		}
+		return resp, err
 	}
-	return nil
 }
 
-// DialOption grpc client option
-func DialOption(tracer opentracing.Tracer) grpc.DialOption {
-	return grpc.WithUnaryInterceptor(clientInterceptor(tracer))
-}
-
-// NewTracer NewJaegerTracer for current service
-func NewTracer(serviceName string, agentHost string) (tracer opentracing.Tracer, closer io.Closer, err error) {
-
-	metricsFactory := prometheus.New()
-
-	cfg := jaegercfg.Configuration{
-		ServiceName: serviceName,
-		Sampler: &jaegercfg.SamplerConfig{
-			Type:  "const",
-			Param: 1,
-		},
-		Reporter: &jaegercfg.ReporterConfig{
-			LogSpans:            true,
-			BufferFlushInterval: 1 * time.Second,
-			LocalAgentHostPort:  agentHost,
-		},
-	}
-	tracer, closer, err = cfg.NewTracer(
-		jaegercfg.Metrics(metricsFactory),
-		jaegercfg.Logger(jaeger.StdLogger),
+// NewRecoveryInterceptor grpc server ServerInterceptor
+// create union recovery handler for gRPC service
+func NewRecoveryInterceptor(logfile string) grpc.UnaryServerInterceptor {
+	var (
+		errorLogger io.Writer
+		err         error
+		startTime   time.Time
+		remoteIp    string
 	)
 
+	if logfile != "" {
+		errorLogger, err = logkit.NewFileLogger(logfile, "", time.Second, uint64(1204*1024*1800), 0)
+	} else {
+		errorLogger, err = os.Open("/dev/stderr")
+	}
 	if err != nil {
-		return
+		println("logger conf fail, %s", err.Error())
 	}
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
 
-	opentracing.SetGlobalTracer(tracer)
-	return
-}
-
-//clientInterceptor grpc client wrapper
-func clientInterceptor(tracer opentracing.Tracer) grpc.UnaryClientInterceptor {
-
-	return func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
-
-		var parentCtx opentracing.SpanContext
-		parentSpan := opentracing.SpanFromContext(ctx)
-		if parentSpan != nil {
-			parentCtx = parentSpan.Context()
-		}
-
-		span := tracer.StartSpan(
-			method,
-			opentracing.ChildOf(parentCtx),
-			opentracing.Tag{Key: string(ext.Component), Value: "gRPC"},
-			ext.SpanKindRPCClient,
-		)
-		defer span.Finish()
-
-		md, ok := metadata.FromOutgoingContext(ctx)
-		if !ok {
-			md = metadata.New(nil)
-		} else {
-			md = md.Copy()
-		}
-
-		mdWriter := MDReaderWriter{md}
-		err := tracer.Inject(span.Context(), opentracing.TextMap, mdWriter)
-		if err != nil {
-			span.LogFields(log.String("inject-error", err.Error()))
-		}
-
-		newCtx := metadata.NewOutgoingContext(ctx, md)
-		err = invoker(newCtx, method, req, reply, cc, opts...)
-		if err != nil {
-			span.LogFields(log.String("call-error", err.Error()))
-		}
-		return err
-	}
-}
-
-//serverInterceptor grpc server wrapper
-func serverInterceptor(tracer opentracing.Tracer) grpc.UnaryServerInterceptor {
-	return func(ctx context.Context,
-		req interface{},
-		info *grpc.UnaryServerInfo,
-		handler grpc.UnaryHandler) (resp interface{}, err error) {
-
-		md, ok := metadata.FromIncomingContext(ctx)
-		if !ok {
-			md = metadata.New(nil)
-		}
-
-		spanContext, err := tracer.Extract(opentracing.TextMap, MDReaderWriter{md})
-		if err != nil && err != opentracing.ErrSpanContextNotFound {
-			logkit.Errorf("extract from metadata err: %v", err)
-		} else {
-			span := tracer.StartSpan(
-				info.FullMethod,
-				ext.RPCServerOption(spanContext),
-				opentracing.Tag{Key: string(ext.Component), Value: "gRPC"},
-				ext.SpanKindRPCServer,
-			)
-			defer span.Finish()
-
-			ctx = opentracing.ContextWithSpan(ctx, span)
-		}
-
+		defer func() {
+			if r := recover(); r != nil {
+				if _, err := errorLogger.Write([]byte(fmt.Sprintf("%s\t%s\t%s\n fatal ===> %s %s \n===============\n", startTime.Format(time.RFC3339), remoteIp, info.FullMethod, r, debug.Stack()))); err != nil {
+					println("write access log fail, %s", err.Error())
+				}
+				err = status.Errorf(codes.Internal, "fatal err: %+v", r)
+			}
+		}()
 		return handler(ctx, req)
 	}
+}
+
+// NewTracerInterceptor grpc server ServerInterceptor
+func NewTracerInterceptor(serviceName string, agentHost string) grpc.UnaryServerInterceptor {
+	var (
+		err error
+	)
+	if agentHost != "" {
+		QTracer, _, err = NewJaegerTracer(serviceName, agentHost)
+	} else {
+		QTracer, _, err = NewTracer(serviceName)
+	}
+	if err != nil {
+		return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+			return handler(ctx, req)
+		}
+	}
+	opentracing.SetGlobalTracer(QTracer)
+	return serverInterceptor(QTracer, true)
 }
